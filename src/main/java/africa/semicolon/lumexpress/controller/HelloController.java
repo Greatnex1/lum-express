a7382eef4d5d90f0e8d47ee0494a44ba4b066883
Nouah <noahgreatakoni@gmail.com>
2022-09-22 10:10:25 +0100

    spring boot code web

---
+++ src/main/java/africa/semicolon/lumexpress/controller/HelloController.java
@@ -0,0 +1,27 @@
+package africa.semicolon.lumexpress.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+        @GetMapping("/hello")
+
+    public String hello(){
+            return "Hello";
+        }
+
+    @GetMapping("/come")
+
+    public String come(){
+            return "arrive";
+    }
+
+    @GetMapping("/welcome")
+
+    public String welcome(){
+        return "welcome";
+    }
+}
